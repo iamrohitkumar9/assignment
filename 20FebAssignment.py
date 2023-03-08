b0b35aa3f5cf1473e15339ec3af621692a15460a
iamrohitkumar9 <123239825+iamrohitkumar9@users.noreply.github.com>
2023-03-08 11:18:45 +0530

    Add files via upload

---
+++ 20FebAssignment.py
@@ -0,0 +1,37 @@
+# Question 1 :
+
+# GET: The GET method is used to request data from a specified resource. It is one 
+# of the most common HTTP methods and is used to retrieve data from a server. The GET 
+# method sends the encoded user information appended to the page request. The page and the 
+# encoded information are separated by the '?' character.
+
+# POST: The POST method is used to send data to a server to create or update a resource. 
+# It is often used when submitting an HTML form. The data sent to the server with POST is 
+# stored in the request body of the HTTP request. Unlike GET, POST is not limited by length 
+# restrictions and can be used to send large amounts of data.
+
+# Question 2 :
+
+# Request is used in Flask to access the request data sent by the client, such as form data,
+# files, and headers. It also provides access to the metadata of the request, such as the 
+# remote address of the client and the URL scheme of the request.
+
+# Question 3 :
+
+# Redirect() is used in Flask to redirect a user from one URL to another. This is often 
+# used when a user has submitted a form or completed an action that requires them to be
+# redirected to another page. It can also be used to redirect users from an old URL to a 
+# new one, or from an insecure (HTTP) page to a secure (HTTPS) page.
+
+# Question 4 :
+
+# Templates in Flask are HTML files that contain placeholders for dynamic data. The 
+# render_template() function is used to render the template with the given arguments and 
+# return the rendered template string. This allows us to create dynamic webpages that can be
+# populated with data from a database or other sources.
+
+# Question 5 :
+
+# Unable to upload the screenshot so shared google drive link so please consider this 
+# gdrive link : https://drive.google.com/file/d/1AM9Uvx_rh7rVyUdN3GN5QnQyN28cQ5xW/view?usp=sharing
+
